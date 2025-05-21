439fd9d65c19e6e69300a98aa5650025f4dc306e
25LP4381 25LP4381 <25LP4381@admins-iMac.local>
2025-05-21 15:10:53 +0800

    aldaa

--- src/components/Upcoming.jsx
+++ src/components/Upcoming.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import { MovieCard } from "./MovieCard";
 import { Button } from "./ui/button";
 import { MoveRight } from "lucide-react";
+import { getUpcomingMovies } from "@/pages/api/get-upcoming-movie";
 export const Upcoming = () => {
+  const [upcomingMovies, setUpcomingMovies] = useState([])
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const upcomingMovies = await getUpcomingMovies();
+      setUpcomingMovies (upcomingMovies);
+    }
+  })
   return (
     <div className="flex flex-col gap-8">
       <div className="flex justify-between">
@@ -12,8 +21,8 @@ export const Upcoming = () => {
         </Button>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <MovieCard />
+        {upcomingMovies?.map((movie) => (
+          <MovieCard key={movie.id} movie={movie}/>
         ))}
       </div>
     </div>
